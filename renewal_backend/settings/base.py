b071e98dca0117f41ec9da0fd737f224d7cabe86
Sahina1707 <Sahinayasin17@gmail.com>
2025-07-24 18:25:06 +0530

    evecommit

--- renewal_backend/settings/base.py
+++ renewal_backend/settings/base.py
@@ -292,8 +292,8 @@ EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.
 EMAIL_HOST = config('EMAIL_HOST', default='localhost')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='sahinayasin17@gmail.com')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='qnfk rbav tjmv wktv')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@intelipro.com')
 ADMIN_EMAIL = config('ADMIN_EMAIL', default='admin@intelipro.com')
 
